32ba55eb0a752b7d7c842026dec4d0cddaf05887
Prodysa <bcmauricio.rivera@gmail.com>
2025-05-20 12:47:51 -0700

    Actualización de sección Hero y logo menu hamburguesa amarillo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 import React from "react";
-import portada from "../assets/portada.png"; // Asegúrate que esté en /assets
+import portada from "../assets/portada.png";
 import logoColor from "../assets/LOGO COLOR.png";
-
 
 export default function Hero() {
   return (
@@ -19,23 +18,25 @@ export default function Hero() {
         <img
           src={logoColor}
           alt="Multisuspensiones del Norte"
-          className="mx-auto h-48 md:h-60 mb-8 drop-shadow-2xl"
-          style={{ maxWidth: "320px" }} // Esto te asegura control en pantallas grandes
+          className="mx-auto h-60 md:h-72 mb-10"
+          style={{ maxWidth: "400px" }}
         />
         <h1 className="text-5xl md:text-6xl font-extrabold uppercase tracking-tight">
           <span className="text-yellow-400">¡Expertos </span>
-          <span className="bg-white text-black px-2">en</span>{" "}
-          <span className="text-yellow-400">Suspensión!</span>
+          <span className="bg-yellow-400 text-black px-3">EN</span>{" "}
+          <span className="text-yellow-400 text-[1.25em]">SUSPENSIÓN!</span>
         </h1>
         <p className="mt-6 text-lg md:text-xl max-w-xl mx-auto text-white">
           Especialistas en suspensión y dirección automotriz
         </p>
         <a
           href="#about"
-          className="inline-block mt-8 bg-white text-black font-bold text-lg px-8 py-3 transform -skew-x-12 hover:scale-105 transition"
+          className="inline-block mt-8 bg-yellow-400 text-black font-bold text-lg px-8 py-3 transform -skew-x-12 hover:scale-105 transition"
         >
           VER MÁS
         </a>
+
+
       </div>
     </section>
   );
